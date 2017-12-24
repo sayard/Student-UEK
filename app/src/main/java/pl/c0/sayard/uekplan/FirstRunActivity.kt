10ead7d900b943ca976879e23d691f0bc78b2add
Unknown <graczu42@gmail.com>
2017-12-24 21:05:34 +0100

    FirstRunActivity

---
+++ app/src/main/java/pl/c0/sayard/uekplan/FirstRunActivity.kt
@@ -0,0 +1,12 @@
+package pl.c0.sayard.uekplan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstRunActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_run)
+    }
+}
--- app/src/main/java/pl/c0/sayard/uekplan/MainActivity.kt
+++ app/src/main/java/pl/c0/sayard/uekplan/MainActivity.kt
@@ -1,11 +1,16 @@
 package pl.c0.sayard.uekplan
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var prefs: SharedPreferences? = null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -27,8 +32,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        prefs = getSharedPreferences("pl.c0.sayard.uekplan", Context.MODE_PRIVATE)
+
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        var firstRun = prefs?.getBoolean("firstRun", true)
+
+        if(firstRun!!){
+            val intent = Intent(this, FirstRunActivity::class.java)
+            startActivity(intent)
+            prefs?.edit()?.putBoolean("firstRun", false)?.apply()
+        }
     }
 }
