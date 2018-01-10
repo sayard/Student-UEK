2f2c2a0e9fc4a7c8437475d82ec247f7c7f6603b
Unknown <graczu42@gmail.com>
2018-01-10 10:29:15 +0100

    first run check fix

--- app/src/main/java/pl/c0/sayard/uekplan/MainActivity.kt
+++ app/src/main/java/pl/c0/sayard/uekplan/MainActivity.kt
@@ -37,23 +37,19 @@ class MainActivity : AppCompatActivity() {
 
         prefs = getSharedPreferences("pl.c0.sayard.uekplan", Context.MODE_PRIVATE)
 
-        setContentView(R.layout.activity_main)
-
-        navigation.selectedItemId = R.id.navigation_schedule
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.main_frame, ScheduleFragment.newInstance())
-        transaction.commit()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
         val firstRun = prefs?.getBoolean("firstRun", true)
 
         if(firstRun!!){
             val intent = Intent(this, FirstRunStepOneActivity::class.java)
             startActivity(intent)
+        }else{
+            setContentView(R.layout.activity_main)
+
+            navigation.selectedItemId = R.id.navigation_schedule
+            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.main_frame, ScheduleFragment.newInstance())
+            transaction.commit()
         }
     }
 }
