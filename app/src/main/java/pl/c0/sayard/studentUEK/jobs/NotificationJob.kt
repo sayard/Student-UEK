9c54f38efedab9b594a96fe62023750a46c0847f
Unknown <graczu42@gmail.com>
2018-03-22 19:01:24 +0100

    changed data send to notificationjob

--- app/src/main/java/pl/c0/sayard/studentUEK/jobs/NotificationJob.kt
+++ app/src/main/java/pl/c0/sayard/studentUEK/jobs/NotificationJob.kt
@@ -7,6 +7,7 @@ import com.evernote.android.job.Job
 import com.evernote.android.job.JobRequest
 import com.evernote.android.job.util.support.PersistableBundleCompat
 import pl.c0.sayard.studentUEK.R
+import pl.c0.sayard.studentUEK.data.ScheduleItem
 
 /**
  * Created by karol on 08.03.18.
@@ -17,13 +18,27 @@ class NotificationJob: Job() {
 
     override fun onRunJob(params: Params): Result {
         val extras = params.extras
-        val subject = extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_SUBJECT), "")
-        val classroom = extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_CLASSROOM), "")
+        val scheduleItem = ScheduleItem(
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_SUBJECT), ""),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_TYPE), ""),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_TEACHER), ""),
+                extras.getInt(context.getString(R.string.EXTRA_NOTIFICATION_TEACHER_ID), 0),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_CLASSROOM), ""),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_COMMENTS), ""),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_DATE), ""),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_START_DATE), ""),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_END_DATE), ""),
+                false,
+                extras.getBoolean(context.getString(R.string.EXTRA_NOTIFICATION_IS_CUSTOM), false),
+                extras.getInt(context.getString(R.string.EXTRA_NOTIFICATION_CUSTOM_ID), -1),
+                extras.getInt(context.getString(R.string.EXTRA_NOTIFICATION_NOTE_ID), -1),
+                extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_NOTE_CONTENT), "")
+        )
         val hour = extras.getString(context.getString(R.string.EXTRA_NOTIFICATION_HOUR), "")
         val notification = NotificationCompat.Builder(context, context.getString(R.string.SCHEDULE_ITEM_NOTIFICATION_CHANNEL))
                 .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setContentTitle(subject)
-                .setContentText("$classroom $hour")
+                .setContentTitle("${scheduleItem.subject} - ${scheduleItem.type}")
+                .setContentText("${scheduleItem.classroom} $hour")
                 .build()
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(SCHEDULE_ITEM_NOFITICATION_ID, notification)
