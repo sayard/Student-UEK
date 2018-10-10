bf077d2d13d1f4da95e1c0e1ffc7bc30b2a3f5d1
Unknown <graczu42@gmail.com>
2018-10-10 17:42:31 +0200

    notification

--- app/src/main/java/pl/c0/sayard/studentUEK/services/PigeonMessagingService.kt
+++ app/src/main/java/pl/c0/sayard/studentUEK/services/PigeonMessagingService.kt
@@ -1,11 +1,22 @@
 package pl.c0.sayard.studentUEK.services
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import pl.c0.sayard.studentUEK.R
+import pl.c0.sayard.studentUEK.activities.MainActivity
 import pl.c0.sayard.studentUEK.data.Message
 import pl.c0.sayard.studentUEK.db.DatabaseManager
 
 class PigeonMessagingService : FirebaseMessagingService() {
+
+    val PIGEON_NOFITICATION_ID = 420
 
     override fun onMessageReceived(rm: RemoteMessage?) {
         if(rm != null){
@@ -18,8 +29,24 @@ class PigeonMessagingService : FirebaseMessagingService() {
             )
             val dbManager = DatabaseManager(this)
             dbManager.addMessageToDb(message)
+            val intent = Intent(this, MainActivity::class.java).apply{
+                putExtra("EXTRA_OPEN_MESSAGES", true)
+            }
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+            val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val notification = NotificationCompat.Builder(this, getString(R.string.PIGEON_NOTIFICATION_CHANNEL))
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle("${message.title} - ${message.author}")
+                    .setContentText(message.body)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .setVibrate(arrayOf<Long>(0, 200, 60, 200, 60, 200, 200, 60, 200).toLongArray())
+                    .setLights(Color.BLUE, 500, 500)
+                    .setSound(alarmSound)
+                    .build()
+            val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(PIGEON_NOFITICATION_ID, notification)
         }
-        //TODO notification
     }
 
 }
